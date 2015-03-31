d9d2d1b1d600525341db8ab9a3c2ad1f249d6f1d
Adrie Baselmans <adriebaselmans@gmail.com>
2015-03-31 21:01:54 +0200

    Added example of usage in WPF scenerio

---
+++ Observer/ReflectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Observer
+{
+    public static class ReflectionHelper
+    {
+        public static string GetPropertyName(Expression<Func<object>> exp)
+        {
+            var body = exp.Body as MemberExpression;
+
+            if (body == null)
+            {
+                var ubody = (UnaryExpression)exp.Body;
+                body = ubody.Operand as MemberExpression;
+
+                if (body != null)
+                {
+                    return body.Member.Name;
+                }
+            }
+
+            return null;
+        }        
+    }
+}
---
+++ Tests/NotifyPropertyChangedTest.cs
@@ -0,0 +1,75 @@
+using System.ComponentModel;
+using NUnit.Framework;
+using Observer;
+
+namespace Tests
+{
+    /// <summary>
+    /// Example of a Model class
+    /// </summary>
+    public class Model
+    {
+        public WeakProperty<bool> MyWeakBooleanProperty = new WeakProperty<bool>();
+    }
+
+    /// <summary>
+    /// Example ViewModel class, which exposes a property of a model to the view using a 'proxy' property.
+    /// </summary>
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private readonly Model _model;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel(Model model)
+        {
+            _model = model;
+
+            // The ViewModel should subscribe to notifications of the Model's weak property (MyWeakBooleanProperty) and notify the View 
+            // using the INotifyPropertyChanged interface, via the ViewModels 'proxy' property.
+            model.MyWeakBooleanProperty.Subscribe((o, b) => OnPropertyChanged(ReflectionHelper.GetPropertyName(() => MyProperty)));
+        }
+
+        public bool MyProperty
+        {
+            get { return _model.MyWeakBooleanProperty.Value; }
+            set { _model.MyWeakBooleanProperty.Value = value; /*No OnPropertyChanged here, this is handled via the Model via the subscription to the Model's property in the constructor.*/ }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    /// <summary>
+    /// Example of use in an MVVM application.
+    /// 
+    /// In an MVVM application, normal events are typically used in the Model classes. This test illustrates how
+    /// we can still expose the WeakProperty in the Model via the View by using a 'proxy' property and subscription.
+    /// <see cref="ViewModel"/> and <see cref="Model"/>
+    /// </summary>
+    [TestFixture]
+    public class NotifyPropertyChangedTest
+    {
+        [Test]
+        public void GivenWeakProperty_WhenUsedInViewModel_PropertyNotifiesView()
+        {
+            var model = new Model();
+            var viewModel = new ViewModel(model);
+
+            int notificationCount = 0;
+            viewModel.PropertyChanged += (sender, args) =>
+                {
+                    if (args.PropertyName.Equals(ReflectionHelper.GetPropertyName(() => viewModel.MyProperty))) notificationCount++;
+                };
+
+            Assert.AreEqual(0, notificationCount);
+
+            model.MyWeakBooleanProperty.Value = !model.MyWeakBooleanProperty.Value;
+            Assert.AreEqual(1, notificationCount);
+
+            viewModel.MyProperty = !viewModel.MyProperty;
+            Assert.AreEqual(2, notificationCount);
+        }
+    }
+}
